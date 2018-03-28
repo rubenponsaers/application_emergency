1951df8cffbaf9b75cdc70c21709f81d2e67c68a
DESKTOP-F97TEA3\Ruben <rubenponsaers@gmail.com>
2018-03-28 12:17:34 +0200

    Menu item settings ingevoerd Momenteel nog maar 1 preference (dark theme), vernadert nog niet daadwerkelijk het thema

--- Project/app/src/main/java/com/example/android/emergency/MainActivity.java
+++ Project/app/src/main/java/com/example/android/emergency/MainActivity.java
@@ -4,39 +4,60 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button messageHistory;
-    private Button help;
+    private Button emergencyHistory;
+    private Button sendEmergency;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        messageHistory = (Button) findViewById(R.id.button_messageHistory);
-        messageHistory.setOnClickListener(new View.OnClickListener(){
+        //Button actions
+        emergencyHistory = (Button) findViewById(R.id.button_emergencyHistory);
+        emergencyHistory.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 Context context = MainActivity.this;
-                Class destinationActivity = Activity_MessageHistory.class;
-                Intent start_MessageHistory = new Intent(context,destinationActivity);
-                startActivity(start_MessageHistory);
+                Class destinationActivity = EmergencyHistoryActivity.class;
+                Intent startEmergencyHistory = new Intent(context,destinationActivity);
+                startActivity(startEmergencyHistory);
             }
         });
 
-        help = (Button) findViewById(R.id.button_help);
-        help.setOnClickListener(new View.OnClickListener(){
+        sendEmergency = (Button) findViewById(R.id.button_sendEmergency);
+        sendEmergency.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 Context context = MainActivity.this;
-                Toast messageToast = Toast.makeText(context, "Sending message...", Toast.LENGTH_SHORT);
+                Toast messageToast = Toast.makeText(context, "Sending emergency message...", Toast.LENGTH_SHORT);
                 messageToast.show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings){
+            Intent startSettings = new Intent(this, SettingsActivity.class);
+            startActivity(startSettings);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
